8da767dfb4aafe2748e6b78cf536a16252db3b70
Usuario <Usuario@Familia-Herrn-Castao>
2022-09-30 18:01:50 +0200

    fix

--- BootCamp_Esplai2022/src/ComentarioLinea.java
+++ BootCamp_Esplai2022/src/ComentarioLinea.java
@@ -1,12 +1,9 @@
-public class ComentarioVariasLineas {
+public class ComentarioLinea {
 
 	public static void main(String[] args) {
-		/*System.out.println("Hola");
+		
+			//System.out.println("Hola ");
 		System.out.println("Adios");
-		System.out.println("Cucu");*/
-		for(int i=0;i<6;/*ahora viene el paso */ i++)
-			System.out.println(i+" ");
-
 		System.out.println("FIN DE PROGRAMA");
 	}
 
